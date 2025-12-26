abc4e8500bef60b69f63edb4781c60eed754d76a
Recrusion <entertohacking@gmail.com>
2025-12-26 16:54:43 +0300

    init

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,11 +7,13 @@ import (
 	"github.com/Telegram-bot-for-register-on-events/event-service/internal/config"
 )
 
+// App описывает микросервис целиком
 type App struct {
 	GRPCServer *grpcserver.App
 	cfg        *config.Config
 }
 
+// NewApp конструктор для App
 func NewApp(log *slog.Logger) *App {
 	cfg := config.MustLoadConfig(log)
 	grpcApp := grpcserver.New(log, cfg.GetGRPCServerPort())
