69b8d4ce2c908785f9064e8fb5fbf3e4bf8b25a9
Recrusion <entertohacking@gmail.com>
2025-12-26 20:23:14 +0300

    added init storage-layer

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Telegram-bot-for-register-on-events/event-service/internal/app/grpc"
 	"github.com/Telegram-bot-for-register-on-events/event-service/internal/config"
+	"github.com/Telegram-bot-for-register-on-events/event-service/internal/storage/postgres"
 )
 
 // App описывает микросервис целиком
@@ -16,7 +17,11 @@ type App struct {
 // NewApp конструктор для App
 func NewApp(log *slog.Logger) *App {
 	cfg := config.MustLoadConfig(log)
-	grpcApp := grpcserver.New(log, cfg.GetGRPCServerPort())
+	db, err := postgres.NewStorage(log, cfg.GetDatabaseDriverName(), cfg.GetDatabasePath())
+	if err != nil {
+		panic(err)
+	}
+	grpcApp := grpcserver.New(log, cfg.GetGRPCServerPort(), db)
 	return &App{
 		GRPCServer: grpcApp,
 		cfg:        cfg,
