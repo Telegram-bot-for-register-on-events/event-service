9178f31af19e2118c6a718c136ce7b4918e9b326
Recrusion <entertohacking@gmail.com>
2025-12-26 16:53:33 +0300

    init

--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,29 +1,67 @@
-package app
+package grpcserver
 
 import (
+	"fmt"
 	"log/slog"
+	"net"
 
 	eventgrpc "github.com/Telegram-bot-for-register-on-events/event-service/internal/grpc"
 	"google.golang.org/grpc"
 )
 
-// App описывает серверное gRPC-приложение
+// Константы для описания операций
+const (
+	opStart = "grpcserver.Start"
+	opStop  = "grpcserver.Stop"
+)
+
+// App описывает gRPC-сервер
 type App struct {
 	log        *slog.Logger
 	gRPCServer *grpc.Server
 	port       string
 }
 
-// NewApp создаёт новое серверное gRPC-приложение
-func NewApp(log *slog.Logger, port string) *App {
-	// Создаём новый gRPC-сервер
+// New создаёт новый gRPC-сервер
+func New(log *slog.Logger, port string) *App {
 	grpcServer := grpc.NewServer()
-	// Подключаем к нему обработчик
+	// Подключаем обработчик
 	eventgrpc.Register(grpcServer)
-
 	return &App{
 		log:        log,
 		gRPCServer: grpcServer,
 		port:       port,
 	}
+}
+
+// start запускает gRPC-сервер
+func (a *App) start() error {
+	// Инициализируем tcp-слушателя на указанном порту
+	listener, err := net.Listen("tcp", ":"+a.port)
+	if err != nil {
+		return fmt.Errorf("error starting gRPC server - %w", err)
+	}
+
+	a.log.Info("running gRPC-server...", slog.String("operation", opStart), slog.String("port", a.port))
+
+	// Принимаем входящие соединения от слушателя
+	if err = a.gRPCServer.Serve(listener); err != nil {
+		a.log.Info("error starting gRPC server", slog.String("error", err.Error()))
+		return fmt.Errorf("error starting gRPC server - %w", err)
+	}
+
+	return nil
+}
+
+// MustRun обёртка для метода start, при ошибке - паникует
+func (a *App) MustRun() {
+	if err := a.start(); err != nil {
+		panic(err)
+	}
+}
+
+// Stop останавливает gRPC-сервер
+func (a *App) Stop() {
+	a.log.Info("gRPC-server is stopping", slog.String("operation", opStop), slog.String("port", a.port))
+	a.gRPCServer.GracefulStop()
 }
